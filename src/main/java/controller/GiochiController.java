f79fc7681c8f5fa9ae27c297567ecab986324f5c
flarocca <francescolarocca0895@gmail.com>
2022-10-04 21:37:04 +0200

    lista giochi per ruolo

--- src/main/java/controller/GiochiController.java
+++ src/main/java/controller/GiochiController.java
@@ -1,5 +1,27 @@
 package controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import model.GiochiEntity;
+import repository.GiocoRepository;
+
+@Controller
 public class GiochiController {
 
+    @Autowired
+    private GiocoRepository giocoRepository;
+
+    @GetMapping("/giochi")
+    public ModelAndView giochi(@RequestParam String genere) {
+        List<GiochiEntity> giochi = (List<GiochiEntity>) giocoRepository.findAll();
+        ModelAndView viewModel = new ModelAndView("giochi");
+        viewModel.addObject("giochi", giochi);
+        return viewModel;
+    }
 }
--- src/main/java/model/GiochiEntity.java
+++ src/main/java/model/GiochiEntity.java
@@ -1,10 +1,12 @@
 package model;
 
+import java.util.Date;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 
@@ -16,32 +18,44 @@ public class GiochiEntity {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 	
+    @Column(name = "NOME")
 	private String nome;
 	
-	
-	
-	@ManyToOne()
-	private GenereEntity genere;
-	
-	
-	
+    @Column(name = "FK_GENERE")
+    private long fkGenere;
+    @Column(name = "DATA_USCITA")
+    private Date dataUscita;
 
-	public long getId() {
-		return id;
-	}
-	public void setId(long id) {
-		this.id = id;
-	}
-	public GenereEntity getGenere() {
-		return genere;
-	}
-	public void setGenere(GenereEntity genere) {
-		this.genere = genere;
-	}
-	public String getNome() {
-		return nome;
-	}
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public long getFkGenere() {
+        return fkGenere;
+    }
+
+    public void setFkGenere(long fkGenere) {
+        this.fkGenere = fkGenere;
+    }
+
+    public Date getDataUscita() {
+        return dataUscita;
+    }
+
+    public void setDataUscita(Date dataUscita) {
+        this.dataUscita = dataUscita;
+    }
+
 }
